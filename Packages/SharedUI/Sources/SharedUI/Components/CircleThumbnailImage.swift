59709aceda991fe68bb8a4c97c5858e1d25ab731
Chau Boon Yong <rycbn@hotmail.com>
2021-02-08 12:44:35 +0000

    add rounded rectangle and circle thumbnail image components

---
+++ Packages/SharedUI/Sources/SharedUI/Components/CircleThumbnailImage.swift
@@ -0,0 +1,41 @@
+import SwiftUI
+import Kingfisher
+
+public struct CircleThumbnailImage: View {
+    private let shape = Circle()
+    private let url: URL?
+
+    public init(_ url: URL? = nil) {
+        self.url = url
+    }
+    
+    public var body: some View {
+        Group {
+            switch url {
+            case .none:
+                ThumbnailImagePlaceholder()
+            case .some:
+                KFImage(url)
+                    .placeholder {
+                        ThumbnailImagePlaceholder()
+                    }
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+            }
+        }
+        .clipShape(shape)
+        .frame(width: 150, height: 150)
+    }
+}
+
+struct CircleThumbnailImage_Previews: PreviewProvider {
+    private static let url = URL(string: "https://m.media-amazon.com/images/M/MV5BNzQ2ODY0MTIwMV5BMl5BanBnXkFtZTcwNDQ2NzMzMw@@._V1_UX178_CR0,0,178,264_AL_.jpg")!
+
+    static var previews: some View {
+        Group {
+            CircleThumbnailImage(url)
+            CircleThumbnailImage()
+        }
+        .previewLayout(.sizeThatFits)
+    }
+}
---
+++ Packages/SharedUI/Sources/SharedUI/Components/RoundedRectangleThumbnailImage.swift
@@ -0,0 +1,41 @@
+import SwiftUI
+import Kingfisher
+
+public struct RoundedRectangleThumbnailImage: View {
+    private let shape = RoundedRectangle(cornerRadius: 16, style: .continuous)
+    private let url: URL?
+
+    public init(_ url: URL? = nil) {
+        self.url = url
+    }
+    
+    public var body: some View {
+        Group {
+            switch url {
+            case .none:
+                ThumbnailImagePlaceholder()
+            case .some:
+                KFImage(url)
+                    .placeholder {
+                        ThumbnailImagePlaceholder()
+                    }
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+            }
+        }
+        .clipShape(shape)
+        .frame(width: 150, height: 245)
+    }
+}
+
+struct RoundedRectangleThumbnailImage_Previews: PreviewProvider {
+    private static let url = URL(string: "https://m.media-amazon.com/images/M/MV5BNzQ2ODY0MTIwMV5BMl5BanBnXkFtZTcwNDQ2NzMzMw@@._V1_UX178_CR0,0,178,264_AL_.jpg")!
+
+    static var previews: some View {
+        Group {
+            RoundedRectangleThumbnailImage(url)
+            RoundedRectangleThumbnailImage()
+        }
+        .previewLayout(.sizeThatFits)
+    }
+}

b774c45693069381cfe75b4071f7c44fa988e547
Chau Boon Yong <rycbn@hotmail.com>
2021-02-23 23:02:02 +0000

    tidy up AsyncImage lib - indentation and naming convention

--- findmypast/Sources/AsyncImage/AsyncImage.swift
+++ findmypast/Sources/AsyncImage/AsyncImage.swift
@@ -2,23 +2,23 @@ import SwiftUI
 import Combine
 
 final class ImageLoader: ObservableObject {
-  @Published var image: UIImage? = nil
-  private var cancellable: AnyCancellable?
-  
-  deinit {
-    cancellable?.cancel()
-  }
-  
-  init(_ url: URL) {
-    cancellable = URLSession
-        .shared
-        .dataTaskPublisher(for: url)
-        .map(\.data)
-        .map(UIImage.init)
-        .receive(on: DispatchQueue.main)
-        .replaceError(with: nil)
-        .assign(to: \.image, on: self)
-  }
+    @Published var image: UIImage? = nil
+    private var cancellable: AnyCancellable?
+    
+    deinit {
+        cancellable?.cancel()
+    }
+    
+    init(_ url: URL) {
+        cancellable = URLSession
+            .shared
+            .dataTaskPublisher(for: url)
+            .map(\.data)
+            .map(UIImage.init)
+            .receive(on: DispatchQueue.main)
+            .replaceError(with: nil)
+            .assign(to: \.image, on: self)
+    }
 }
 
 public struct AsyncImage: View {
@@ -27,14 +27,14 @@ public struct AsyncImage: View {
     private let placeholder = UIImage(named: "placeholder", in: .module, with: .none)!
     private var uiImage: UIImage? { imageLoader.image }
     private var image: Image { Image(uiImage: uiImage ?? placeholder) }
-
+    
     public init(_ url: URL) {
         imageLoader = ImageLoader(url)
         context = Context()
     }
     
     public var body: some View {
-        context.configations.reduce(image) { current, config in
+        context.configuratons.reduce(image) { current, config in
             config(current)
         }
     }
@@ -42,23 +42,22 @@ public struct AsyncImage: View {
 
 extension AsyncImage {
     struct Context {
-        var configations: [(Image) -> Image] = []
+        var configuratons: [(Image) -> Image] = []
         var placeholder: AnyView? = nil
     }
 }
 
 extension AsyncImage {
-    
     public func placeholder<Content: View>(@ViewBuilder _ content: () -> Content) -> Self {
-        let v = content()
+        let view = content()
         var result = self
-        result.context.placeholder = AnyView(v)
+        result.context.placeholder = AnyView(view)
         return result
     }
     
     public func configure(_ block: @escaping (Image) -> Image) -> Self {
         var result = self
-        result.context.configations.append(block)
+        result.context.configuratons.append(block)
         return result
     }
     
